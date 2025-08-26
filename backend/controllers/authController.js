85983e6c8dae9cbd4e3077df92378c0a81978b02
Ayoola Olayemi <ayoyemi982@gmail.com>
2025-08-26 14:36:41 -0600

    Fix case-sensitive import for user model

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const User = require('../models/user');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
